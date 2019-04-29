71a35e6aa1f999a1734a48bb6311b07c89e97f11
Will Davis <twd38@cornell.edu>
2019-04-29 14:46:22 -0400

    dumpfile

--- todolist/settings.py
+++ todolist/settings.py
@@ -77,11 +77,18 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    },
 }
 
 
@@ -137,3 +144,4 @@ MEDIA_URL = "/media/"
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
+
