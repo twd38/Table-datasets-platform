7617a47722fe314eb552bf9148676c3fd1730e84
Will Davis <twd38@cornell.edu>
2019-04-30 12:35:02 -0400

    setting up heroku db

--- todolist/settings.py
+++ todolist/settings.py
@@ -143,6 +143,9 @@ LOGOUT_REDIRECT_URL = '/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
 MEDIA_URL = "/media/"
 
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'static_cdn', 'media_root')
+
+
 
 
 # STATICFILES_DIRS = [
